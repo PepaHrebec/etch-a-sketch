ee676c6eff720ede7424952adfd1c9f394f425d3
Josef Hrebec <hrebejos@fit.cvut.cz>
2022-03-02 10:20:39 +0100

    Add grid, mouse-over effect and reset button

--- script.js
+++ script.js
@@ -1,11 +1,31 @@
 const gridCont = document.querySelector("#gridCont");
+const reset = document.querySelector("#reset")
 
-function loadGrid() {
-    for(let i=1;i<=16;i++) {
-        let gridBox = document.createElement("div");
-        gridBox.textContent=`${i}`;
-        gridCont.appendChild(gridBox).className="gridBox";
+reset.addEventListener("click", () => {
+    resetGrid();
+});
+
+///////////////////////functions//////////////////////////
+
+function loadGrid(input) {
+    for(let i=1;i<=input;i++) {
+        let gridRow = document.createElement("div");
+        gridCont.appendChild(gridRow).className="gridRow";
+        for(let x=1;x<=input;x++) {
+            let gridBox = document.createElement("div");
+            gridBox.addEventListener("mouseenter", () => {
+                gridBox.style.backgroundColor="black";
+            });
+            gridRow.appendChild(gridBox).className="gridBox";
+        }
     }
 }
 
-loadGrid();
+function resetGrid() {
+    const gridBoxes = document.querySelectorAll(".gridBox");
+    gridBoxes.forEach((gridBox) => {
+        gridBox.style.backgroundColor="white";
+    });
+}
+
+loadGrid(16);
